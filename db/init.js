b4caa4755816799e41eb23fdab61e22eb9f31cfe
Amina <minassouane@gmail.com>
2026-02-24 17:23:33 +0100

    Create and populate database

--- db/init.js
+++ db/init.js
@@ -1,17 +1,35 @@
-const pool = require("./pool");
+const { Client } = require("pg");
 
-async function createTable() {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id SERIAL PRIMARY KEY,
+const createTable = `CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
       user_name VARCHAR(255) NOT NULL,
       text TEXT NOT NULL,
       added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-  `);
+  `;
 
-  console.log("✅ messages table created");
-  pool.end();
+const createData = `INSERT INTO messages (user_name, text) VALUES 
+('Lily', 'Hi Harry'), ('James', 'Sup son'), ('Sirius', 'Right here')`;
+
+async function main() {
+  const client = new Client({
+    connectionString: "postgresql://aminaassouane:null@localhost:5432/messages",
+  });
+  try {
+    await client.connect();
+    console.log("Connected to database.");
+    await client.query("DROP TABLE IF EXISTS messages");
+    console.log("Table reset.");
+    await client.query(createTable);
+    console.log("Table created.");
+    await client.query(createData);
+    console.log("Table populated");
+  } catch (error) {
+    console.error("Error occured:", error);
+  } finally {
+    await client.end();
+    console.log("Done.");
+  }
 }
 
-createTable();
+main();
